cc9b2a5ce338172a885cdf85d9b11c0d642f2318
teresa youssef <teresayoussef10@gmail.com>
2023-06-01 11:57:27 -0400

    html confirmación correo y correcciones base del chat
    Falta integrar el htlm con el back. En el chat falta añadir los botones cuando se integre con el back.

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,8 +3,17 @@ import { AppBar, Avatar, IconButton, Stack, Toolbar, Typography, Box, TextField 
 import './chat.css';
 import { useEffect, useState, Fragment, useRef } from "react";
 import { NavBar } from '../components/NavBar';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Chat = (): JSX.Element => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(-1);
+     };
+
 
     return (
         <div>
@@ -12,11 +21,14 @@ const Chat = (): JSX.Element => {
             <div className='cont-chat'>
                 <AppBar 
                     position="fixed" 
-                    sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, position: 'relative', backgroundColor: '#f6f6f6', borderBottom: '1px solid #e0e0e0' }} 
+                    sx={{ zIndex: (theme) => theme.zIndex.drawer, position: 'relative', backgroundColor: '#f6f6f6', borderBottom: '1px solid #e0e0e0' }} 
                     elevation={0}
                 >
                     <Toolbar>
                         <Stack direction="row" spacing={2} sx={{position: 'relative', width: '100%', alignItems: 'center', color: '#010101'}}>
+                            <IconButton onClick={handleClick}>
+                                <ArrowBackIcon/> 
+                            </IconButton>
                             <Avatar imgProps={{ referrerPolicy: "no-referrer" }} sx={{cursor: 'pointer'}} />
                             <Typography noWrap sx={{maxWidth: '300pt', textOverflow: 'ellipsis'}}>
                                 Teresa Youssef
@@ -26,16 +38,12 @@ const Chat = (): JSX.Element => {
                 </AppBar>
                 {/* @ts-ignore */}
                 <div className='cont-messages'>
-                    <div style={{paddingBottom: '15pt'}}>
-                        <Typography variant="body2" textAlign={"center"} color="grayText" sx={{marginTop: '30pt', marginBottom: '-30pt'}}>
-                            prueba
-                        </Typography>
-                    </div>
+                    
                 </div>
                 <div style={{position: 'absolute', bottom: 0, width: '100%'}}>
                     <AppBar 
                         position='fixed'
-                        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#f6f6f6', borderTop: '1px solid #e0e0e0', position: 'relative' }} 
+                        sx={{ zIndex: (theme) => theme.zIndex.drawer, backgroundColor: '#f6f6f6', borderTop: '1px solid #e0e0e0', position: 'relative' }} 
                         elevation={0}
                     >
                         <Toolbar sx={{color: '#010101'}}>
